d50332f994be57a3ad5455c3cda5baf0e88f9b3d
MD Nurul Karim <md-nurul.karim@dfki.de>
2018-11-05 13:01:53 +0100

    KDL tree, chain, fk solver, robot_description subscriber there, working on jacobian

--- src/kdl_menzi_param.cpp
+++ src/kdl_menzi_param.cpp
@@ -3,6 +3,12 @@
 #include <sstream>
 #include <iostream>
 #include <urdf/model.h>
+
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <sensor_msgs/JointState.h>
+
 //#include <tree.hpp>
 
 
@@ -11,10 +17,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "body_joints");
     ros::NodeHandle nh;
-   KDL::Tree my_tree;
+    KDL::Tree my_tree;
    
    //std::string urdf_file = "test_robot.urdf";
-  // KDL::Tree my_tree;
+
    ros::NodeHandle node;
    std::string robot_desc_string;
    node.param("robot_description", robot_desc_string, std::string());
@@ -23,26 +29,31 @@ int main(int argc, char** argv)
       return false;
    }
 
-unsigned int nj = my_tree.getNrOfJoints();
-unsigned int js = my_tree.getNrOfSegments();
+   unsigned int nj = my_tree.getNrOfJoints();
+   unsigned int js = my_tree.getNrOfSegments();
 
-ROS_INFO("Failed to construct kdl tree");
-ROS_INFO("%d   ||  %d \n", nj,js);
+   //ROS_INFO("Failed to construct kdl tree");
+   ROS_INFO("NrOfJoints =%d   ||  NrOfSegments= %d \n", nj,js);
 
 
    KDL::Chain chain;
-//chain= 
- if (!my_tree.getChain("base_link", "bucket", chain)){
+   //chain= 
+   if (!my_tree.getChain("base_link", "bucket", chain)){
       ROS_ERROR("Failed to construct kdl chain");
       return false;
    }
 
-//robot_kin.getChain("base_link", "bucket", chain);
- //  KDL::JntArray joint_pos(chain.getNrOfJoints());
-//  KDL::Frame cart_pos;
-//  KDL::ChainFkSolverPos_recursive fk_solver(chain);
-//  fk_solver.JntToCart(joint_pos, cart_pos);
+   //fk solver= 
+   KDL::ChainFkSolverPos_recursive fksolver = KDL::ChainFkSolverPos_recursive(chain);
 
+   // ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(chain);
+
+
+   //subscribing robot joint angles from gazebo= 
+   const sensor_msgs::JointStateConstPtr initJoints = ros::topic::waitForMessage<sensor_msgs::JointState>("/walking_excavator/joint_states", nh);
+
+   ROS_INFO("Joint States published");
+   ROS_INFO("length of joint states %d", initJoints->name.size()); 
 
 
 }
